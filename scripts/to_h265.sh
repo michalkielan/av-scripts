0fc4e6c7c9015380f2067b07860f21bcebd6265c
Michal Kielan <michalkielan@protonmail.com>
2025-02-07 13:07:11 +0100

    support multiplatform

--- scripts/to_h265.sh
+++ scripts/to_h265.sh
@@ -3,11 +3,23 @@
 AUDIO_CODEC_ACC="libfdk_aac"
 AUDIO_CODEC_PCM_S16="pcm_s16le"
 OUT_DIR="h265"
-FORMAT="mp4"
+FORMAT="MOV"
 AUDIO_CODEC=$AUDIO_CODEC_ACC
 mkdir -p "${OUT_DIR}"
 
-total=$(find . -maxdepth 1 -type f -name '*${FORMAT}' | wc -l)
+PLATFORM=$(uname -s)
+if [[ "$PLATFORM" == "Linux" ]]; then
+  VIDEO_CODEC="hevc_nvenc"
+  VIDEO_OPTIONS="-preset slow -qp 18"
+elif [[ "$PLATFORM" == "Darwin" ]]; then
+  VIDEO_CODEC="hevc_videotoolbox"
+  VIDEO_OPTIONS="-tag:v hvc1 -q:v 65"
+else
+  echo "Error: Unsupported platform - $PLATFORM"
+  exit 1
+fi
+
+total=$(find . -maxdepth 1 -type f -name "*.${FORMAT}" | wc -l)
 i=1
 
 for input_file in *.${FORMAT}
@@ -16,12 +28,12 @@ do
   random_string=$(tr -dc 'a-zA-Z' </dev/urandom | fold -w 8 | head -n 1)
   out_file_tmp="${OUT_DIR}/${input_file}${random_string}.tmp.${FORMAT}"
   out_file="${OUT_DIR}/${input_file}"
+
   if ! ffmpeg \
         -hwaccel auto \
         -i "${input_file}" \
-        -c:v hevc_nvenc \
-        -preset slow \
-        -qp 18 \
+        -c:v ${VIDEO_CODEC} \
+        ${VIDEO_OPTIONS} \
         -c:a ${AUDIO_CODEC} \
         -b:a 320k \
         -map_metadata 0 \
@@ -34,18 +46,3 @@ do
   mv "${out_file_tmp}" "${out_file}"
   ((i++))
 done
-
-# ffmpeg \
-#     -hwaccel auto \
-#     -i "${input_file}" \
-#     -c:v av1_nvenc \
-#     -preset p4 \
-#     -rc vbr_hq \
-#     -cq 30 \
-#     -maxrate 0 \
-#     -b:v 0 \
-#     -c:a ${AUDIO_CODEC} \
-#     -map_metadata 0 \
-#     -loglevel info \
-#     "${out_file_tmp}"
-# 
